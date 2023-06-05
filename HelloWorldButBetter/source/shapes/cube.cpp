4c8b151546eacad21d7fa20aa5df71c5718253e9
Ewan Massonet <ewan.massonet@epitech.eu>
2023-06-05 05:54:09 +0200

    changed fov

--- HelloWorldButBetter/source/shapes/cube.cpp
+++ HelloWorldButBetter/source/shapes/cube.cpp
@@ -42,7 +42,7 @@ void Renderer::cube(glm::vec3 Position, int i)
     float angle = 20.0f * i; 
     model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
     view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
-    projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    projection = glm::perspective(glm::radians(60.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
     this->SHADERS[FLAT3D].shader.setTransform("model", model);
     this->SHADERS[FLAT3D].shader.setTransform("view", view);
